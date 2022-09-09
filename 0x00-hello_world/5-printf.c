1e46e9981d3370ab29b96b1242bd0ec1c0ef2a2a
Ezeilo <ezeilochibuzor143281@gmail.com>
2022-09-09 10:03:57 -0700

    HW 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- entry point for the program.
+ * 
+ * Return: 0 if no error, returns non-zero if errors are present.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
