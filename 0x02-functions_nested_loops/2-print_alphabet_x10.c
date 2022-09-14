b4787663aeb94243b963e2cf95eb336163ffa61e
Ezeilo <ezeilochibuzor143281@gmail.com>
2022-09-14 16:59:20 -0700

    ha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,18 +2,18 @@
 
 /**
  * print_alphabet_x10 - prints the alphabet 10 times
+ * @jay - argument to be passed
  */
-
 void print_alphabet_x10(void)
 {
-	int j;
+	int jay;
 	int x;
 
-	for (x = 0, x < 10, x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (j = 'a', j <= 'z', j++)
+		for (jay  = 'a'; jay <= 'z'; jay++)
 		{
-			_putchar(j);
+			_putchar(jay);
 		}
 		_putchar('\n');
 	}
