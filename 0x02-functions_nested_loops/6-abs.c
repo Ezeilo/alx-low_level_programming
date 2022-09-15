b0f38d72fb1faf3682dee8ef75afcf623199a7a6
Ezeilo <ezeilochibuzor143281@gmail.com>
2022-09-14 17:43:59 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value of number.
+ * @n: parameter
+ *
+ * Return: absolute vakue of number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
