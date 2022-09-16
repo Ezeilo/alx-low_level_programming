594bfe74ed33292530846017e3e3bc2b4bfece3d
Ezeilo <ezeilochibuzor143281@gmail.com>
2022-09-15 20:42:46 -0700

    hallow

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints square
+ * @size: parameter
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int inc1, inc2;
+
+	if (size > 0)
+	{
+		for (inc2 = 0; inc2 < (size - 1); inc2++)
+		{
+			putchar('0');
+		}
+		putchar('0');
+		outchar('\n');
+	}
+}
+else
+{
+putchar('\n');
+}
+
